23dcde981173defa9e431473c2b58851d68a8cb7
Leonardo Nunes <leonardonunesdev@gmail.com>
2026-03-08 19:32:26 -0300

    fix: make GameManager.joinGame thread safe with a Mutex

--- src/main/kotlin/me/leonunes/games/rooksandwalls/model/GameManager.kt
+++ src/main/kotlin/me/leonunes/games/rooksandwalls/model/GameManager.kt
@@ -1,16 +1,19 @@
 package me.leonunes.games.rooksandwalls.model
 
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import me.leonunes.games.common.asId
 import me.leonunes.games.users.User
 
 class GameManager(val game: Game) {
     private val _players = mutableListOf<Player>()
+    private val mutex = Mutex()
     val players: List<Player> get() = _players.toList()
 
-    suspend fun joinGame(user: User): Player {
+    suspend fun joinGame(user: User): Player = mutex.withLock {
         val existingIndex = _players.indexOfFirst { it.id == user.id.asId<Player>() }
         if (existingIndex >= 0) {
-            return reconnect(existingIndex)
+            return@withLock reconnect(existingIndex)
         }
         if (_players.size >= game.config.numberOfPlayers) throw GameFullException()
 
@@ -20,7 +23,7 @@ class GameManager(val game: Game) {
         if (_players.size == game.config.numberOfPlayers) {
             game.start(_players.toList())
         }
-        return player
+        player
     }
 
     suspend fun disconnectPlayer(playerId: PlayerId) {
